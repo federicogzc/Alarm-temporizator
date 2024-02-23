e86839597f63ad88136bd9ec330c5059ea82e56c
Frederick <155497656+federicogzc@users.noreply.github.com>
2024-02-22 20:04:27 -0500

    Update temporizador.py

--- temporizador.py
+++ temporizador.py
@@ -9,14 +9,26 @@ def start_timer(duration):
 
 def play_sound():
     # Asegúrate de actualizar la ruta al archivo MP4 correctamente
-    filepath = "Aquí pones la ruta de tu archivo mp4"
+    filepath = "/mnt/c/Users/Frederick/Desktop/proyectos/Code/itsMylife.mp4"
     # En Linux, utiliza 'xdg-open' para abrir archivos con su programa predeterminado
     os.system(f'xdg-open "{filepath}"')
 
 if __name__ == "__main__":
-    duracion = input("Introduce la duración del temporizador en segundos: ")
+    unidad = input("Introduce la unidad de tiempo (segundos, minutos, horas): ").lower()
+    duracion = input("Introduce la duración del temporizador: ")
+
     try:
         duracion = int(duracion)
+        if unidad == "minutos":
+            duracion *= 60  # Convertir minutos a segundos
+        elif unidad == "horas":
+            duracion *= 3600  # Convertir horas a segundos
+        elif unidad != "segundos":
+            raise ValueError("Unidad de tiempo no reconocida.")
+
         start_timer(duracion)
-    except ValueError:
-        print("Por favor, introduce un número entero como duración.")
+    except ValueError as e:
+        if str(e) == "Unidad de tiempo no reconocida.":
+            print(e)
+        else:
+            print("Por favor, introduce un número entero como duración.")
